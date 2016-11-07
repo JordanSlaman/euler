60683cf20f2874880e477b842eda2ae9a54d4fac
Jordan <jordan.slaman@gmail.com>
2016-11-06 21:38:56 -0800

    Revisiting with some more experience

--- 1.py
+++ 1.py
@@ -5,9 +5,13 @@ If we list all the natural numbers below 10 that are multiples of 3 or 5, we get
 
 Find the sum of all the multiples of 3 or 5 below 1000.
 """
-
+"""
+# Apr 17, 2013
 total = 0
 for x in range(0,1000):
 	if x % 3 == 0 or x % 5 == 0:
 		total += x
 print total
+"""
+# Nov 06, 2016
+print sum(map(lambda x: x if x % 3 == 0 or x % 5 == 0 else 0, range(1000)))
